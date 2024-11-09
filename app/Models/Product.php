5d2fe740c49c40be37ebc5bccc0068c22141fba3
SahirRadzi <sahir.radzi@gmail.com>
2024-11-09 15:32:51 +0800

    setup -m, setting ERD between Category with Product

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    protected $fillable =[
+        'name',
+        'category_id',
+        'slug',
+        'stock',
+        'price',
+        'is_active',
+        'image',
+        'barcode',
+        'description',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+
+}
